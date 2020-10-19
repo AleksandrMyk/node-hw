f8dcc893dc1154cf51c780d444ae49482fe0b6e7
Aleksandr Mykhailov <ss.mykhailov@gmail.com>
2020-10-19 16:16:18 +0300

    ''

--- api/routers/contact.router.js
+++ api/routers/contact.router.js
@@ -3,17 +3,14 @@ const ContactController = require("../controllers/contact.controller");
 
 const router = express.Router();
 
-//get contact list
 router.get("/", ContactController.getUsers);
 
-// create new contact and add it to the list
 router.post(
   "/",
   ContactController.validateCreateUser,
   ContactController.addUser
 );
 
-//update Info about existed contact
 router.patch(
   "/:id",
   ContactController.checkUserInList,
@@ -22,14 +19,12 @@ router.patch(
   ContactController.updateUser
 );
 
-//find contact by ID
 router.get(
   "/:id",
   ContactController.checkUserInList,
   ContactController.getUserById
 );
 
-//delete contact by id
 router.delete(
   "/:id",
   ContactController.checkDataExist,
